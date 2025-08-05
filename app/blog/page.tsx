11b08b1e1969dcbb3757adb5216225c39ad52bae
Zayne <zaynekomichi@hotmail.com>
2025-08-05 11:56:06 +0200

    feat: Add Blog and SubstackFeed components with global type declarations

---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Blog(){
+    return(
+        <div className="container mt-5 mb-5 min-vh-100">
+            <h1 className="fw-bold display-1 tp text-center"> Blog</h1>
+        </div>
+    )
+}
