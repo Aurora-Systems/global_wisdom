778815ffa9479f5b95796a5cbd71b2cd88621d51
Zayne <zaynekomichi@hotmail.com>
2025-08-05 16:10:02 +0200

    feat: Add Shop page with introductory content and animations

---
+++ app/shop/page.tsx
@@ -0,0 +1,21 @@
+import { Fade } from "react-awesome-reveal";
+
+export default function Shop(){
+    return(
+        <div className="Container   mb-5 min-vh-100">
+            <div className="bcc p-5 text-white text-center">
+                <Fade cascade direction="up">
+ <h1 className="display-1 fw-bold ">
+                Book <span className="text-warning">Shop</span>
+            </h1>
+            <p>Discover our curated collection of published works</p>
+            </Fade>
+            
+            </div>
+            <Fade>
+
+            </Fade>
+           
+        </div>
+    )
+}
