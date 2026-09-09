d67b2079377eec1080ac972323b1a3ef3e0312af
Zayne Komchi <54587124+zaynekomichi@users.noreply.github.com>
2026-09-09 08:43:41 +0200

    Add CDN configuration for clipond.com

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,13 @@ const nextConfig: NextConfig = {
             hostname: "ngratesc.sirv.com",
             port: "",
             pathname: "/**"
-        }
+        },
+   {
+     protocol:"https",
+     hostname:"cdn.clipond.com",
+     port:"",
+     pathname:"/**"
+   }
     ]
   }
 
